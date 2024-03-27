5a57f471e80929464a2a4a6e027d19284326e856
danteBenitez <benitezdante123@gmail.com>
2024-03-27 10:58:47 -0300

    fix: Cambiado algoritmo para resolver caso límite

--- missing_number.py
+++ missing_number.py
@@ -3,17 +3,13 @@ def missing_number(count: int, number_list: list[int]) -> int:
         Dada una cantidad de elementos `count`, y una lista que va
         desde 1 hasta `count`, encuentra el primer número faltante.
     """
-    # Por cada número desde cero hasta `count`...
-    for i in range(count):
-        # Como el primer elemento (1) está en la posición cero,
-        # la posición i debe coincidir con i + 1.
-        if number_list[i] != i + 1:
-            # Si no coincide, encontramos el número faltante 
-            # y lo retornamos.
-            return i + 1
+    # El número que falta es la suma de los números de 1 hasta `count`
+    # menos la suma de los que están en la lista.
+    return sum(range(1, count + 1)) - sum(number_list)
 
 assert missing_number(5, [1, 2, 4, 5]) == 3, "Error en el caso de prueba"
 assert missing_number(3, [1, 3]) == 2, "Error en el caso de prueba"
+assert missing_number(3, [1, 2]) == 3, "Error en el caso de prueba"
 assert missing_number(4, [1, 3, 4]) == 2, "Error en el caso de prueba"
 
 print("Todas las pruebas pasaron")
--- weird_algorithm.py
+++ weird_algorithm.py
@@ -6,6 +6,7 @@ def weird_algorithm(start_number: int):
             - Si n es impar, el nuevo valor de n es 3*n + 1
             - Si n es uno, termina el algoritmo.
     """
+    assert start_number > 0, "`start_number` debe ser positivo."
     current_number = start_number
     number_list = [current_number]
     while current_number != 1:
@@ -21,5 +22,8 @@ def weird_algorithm(start_number: int):
     return number_list
 
 assert weird_algorithm(3) == [3, 10, 5, 16, 8, 4, 2, 1], "Error en el caso de prueba"
+assert weird_algorithm(5) == [5, 16, 8, 4, 2, 1], "Error en el caso de prueba"
+assert weird_algorithm(4) == [4, 2, 1], "Error en el caso de prueba"
+
 
 print("Todas las pruebas pasaron.")
